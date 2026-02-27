1ebaa6b8924896d72d973b473c636a384a055f18
Chaos_Seeker <toyashjain09@gmail.com>
2026-02-27 19:52:20 +0530

    Delete js directory

--- js/pwa.js
+++
@@ -1,9 +0,0 @@
-// Minimal PWA helper (safe no-op)
-(function(){
-  // You can add "install" UI later. For now just register sw if present.
-  if('serviceWorker' in navigator){
-    window.addEventListener('load', ()=>{
-      navigator.serviceWorker.register('./sw.js').catch(()=>{});
-    });
-  }
-})();
